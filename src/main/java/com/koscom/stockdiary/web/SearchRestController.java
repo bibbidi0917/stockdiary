e47f73c6a0b5439c7dcfdfff6efa842a0b1d8f41
minjib <minjib79@gmail.com>
2021-11-10 21:05:16 +0900

    controller 이름 변경

--- src/main/java/com/koscom/stockdiary/web/SearchRestController.java
+++ src/main/java/com/koscom/stockdiary/web/SearchRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
-public class SearchController {
+public class SearchRestController {
 
     private final TransService transService;
 
